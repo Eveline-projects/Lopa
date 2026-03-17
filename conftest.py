45db84c62663d56c1f00d3874ca222e6b4bc3ccf
Ewelina <ewelinamast@wp.pl>
2026-03-17 14:42:13 +0100

    Add submission fixture to conftest

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
 import pytest
+from django.contrib.auth.models import User
 from apps.problems.models import Problem
+from apps.engine.models import Submission
+
 
 @pytest.fixture
 def problem(db):
@@ -9,4 +12,21 @@ def problem(db):
         difficulty='easy',
         category='Strings',
 
+    )
+
+
+@pytest.fixture
+def submission(db, problem):
+    user = User.objects.create_user(
+        username='Adam',
+        email='adam@gmail.com',
+        password='1234'
+    )
+    test_code = 'print("Hello World")'
+
+    return Submission.objects.create(
+        user=user,
+        problem=problem,
+        code=test_code,
+        status='done'
     )
