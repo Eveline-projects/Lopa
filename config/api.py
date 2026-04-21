f4676da576952d28b4a5425a51b776e1630ef76c
Ewelina <ewelinamast@wp.pl>
2026-04-21 13:27:57 +0200

    Add submissions API endpoints

--- config/api.py
+++ config/api.py
@@ -1,8 +1,9 @@
 from ninja import NinjaAPI
 from apps.problems.api import router as problems_router
 from apps.test_cases.api import router as test_cases_router
-
+from apps.submissions.api import router as submissions_router
 api = NinjaAPI(title="Lopa API")
 
 api.add_router('/problems', problems_router)
 api.add_router('', test_cases_router)
+api.add_router('', submissions_router)
