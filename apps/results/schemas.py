94642bd0cfd6889e4cafd485e0b36fa141514fff
Ewelina <ewelinamast@wp.pl>
2026-04-23 17:37:35 +0200

    Update result response schemas

---
+++ apps/results/schemas.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+from typing import Literal
+from ninja import Schema
+
+
+class ResultSchema(Schema):
+    id: UUID
+    submission_id: UUID
+    test_case_id: UUID
+    status: Literal[
+        'PENDING',
+        'PASSED',
+        'WRONG_ANSWER',
+        'ERROR',
+        'TIME_LIMIT_EXCEEDED',
+        'RUNTIME_ERROR',
+    ]
+    actual_output: str | None = None
+    execution_time: float | None = None
