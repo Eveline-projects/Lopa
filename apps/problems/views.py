bc333fd567e265cbb0f517b29b43c8b83b63812b
Ewelina <ewelinamast@wp.pl>
2026-04-26 19:59:50 +0200

    Add problems list and detail views

--- apps/problems/views.py
+++ apps/problems/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
+from .models import Problem
 
-# Create your views here.
+
+class ProblemListView(LoginRequiredMixin, ListView):
+    model = Problem
+    template_name = "problems/problem_list.html"
+    context_object_name = "problems"
+
+    def get_queryset(self):
+        return Problem.objects.filter(is_active=True)
+
+
+class ProblemDetailView(LoginRequiredMixin, DetailView):
+    model = Problem
+    template_name = "problems/problem_detail.html"
+    context_object_name = "problem"
+    pk_url_kwarg = "problem_id"
+
+    def get_queryset(self):
+        return Problem.objects.filter(is_active=True)
