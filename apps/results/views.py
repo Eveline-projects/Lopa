aac0cc4ca93b17c9f7b7ed687d52fd638269f3c7
Ewelina <ewelinamast@wp.pl>
2026-04-30 23:30:51 +0200

    Implement ResultListView and results routing

--- apps/results/views.py
+++ apps/results/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from .models import Result
+from apps.submissions.models import Submission
 
-# Create your views here.
+
+class ResultListView(LoginRequiredMixin, ListView):
+    model = Result
+    template_name = 'results/submission_results.html'
+    context_object_name = 'results'
+
+    def get_queryset(self):
+        submission_id = self.kwargs['submission_id']
+        return Result.objects.filter(submission_id=submission_id).order_by(
+            'id'
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['submission'] = Submission.objects.get(
+            id=self.kwargs['submission_id']
+        )
+        return context
